13d67f1704df71b2fdde1fef38fd80e8ab83ac4d
Aramis Damian Perez Hurtado <aramis.perez.hurtado@gmail.com>
2025-10-27 20:27:06 -0300

    Update main.js

--- cliente/main.js
+++ cliente/main.js
@@ -1,6 +1,6 @@
 const url = ''
-cargarAlumnos()
-function cargarAlumnos(){
+cargarLista()
+function cargarLista(){
 
     fetch(url)
     .then(res => res.json())
@@ -9,6 +9,23 @@ function cargarAlumnos(){
     tbody.innerHTML= '';
      for (let student of data) {
     let tr = document.createElement('tr');
+    let aid = document.createElement('td');
+    let nombre = document.createElement('td');
+    let apellido = document.createElement('td');
+    let {id,nombres ,apellidos } = alumno;
+    aid.textContent=id;
+    nombre.textContent = nombres;
+    apellido.textContent = apellidos;
+    tr.append(aid, nombre , apellido);
+    let td = document.createElement('td');
+    for(let text of btns) {
+        let button = document.createElement('button');
+        button.textContent = text;
+        button.onclick =handleClick;
+        td.append(button);
+    }
+    tr.append(td);
+    tbody.append(tr);
 
     }})
 }
