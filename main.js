bb6c1d8a66ec01412dcaaaa4ced3e64762328dfb
Aramis Damian Perez Hurtado <aramis.perez.hurtado@gmail.com>
2025-10-28 20:54:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const mysql = require('mysql');
 const morgan = require('morgan');
 
-
+const PORT = process.env.PORT || 7000;
 
 const app = express();
 
@@ -12,8 +12,20 @@ app.use(morgan('dev'));
 
 const conn = mysql.createConnection({
     host:'localhost',
-    database: 'Base_alumnos'
+    user: 'root',
+    database: 'alumnos'
 });
 
-app.post
+app.post('/api/asistencias', (req , res) => {
+    const { tipo, alumno, materia } = req.body;
+    const data = [tipo, alumno, materia];
+    const q = 'INSERT INTO registros (tipo, alumno, materias) VALUES (?,?,?)'
+    conn.query( data, (err, rs) => {
+        res.status(201).json({msg: 'Alta OK'});
+    });
+});
+
+app.listen(PORT, () => {
+  console.log('Server andando nomás en el puerto 7000');
+});
 
