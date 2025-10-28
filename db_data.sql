3b5111cde3af57033ff3b3dc56404ec7d7a254dc
Aramis Damian Perez Hurtado <aramis.perez.hurtado@gmail.com>
2025-10-28 20:53:48 -0300

    Update db_data.sql

--- db_data.sql
+++ db_data.sql
@@ -1,22 +1,23 @@
 INSERT INTO cursos VALUES
-(NULL,4,3,'Computacion',12);
+(NULL,4,3,'Computacion',12),
 (NULL,5,1,'Computacion',20),
 (NULL,1,12,'Ciclo Basico',9),
 (NULL,3,6,'Computacion',7),
-(NULL,3,8,'Automotor',8)
-;
-
-INSERT INTO alumnos VALUES
-(NULL,'Luna','Estanga',12345678,1),
-(NULL,'Santiago','Villarroel',12345678,3),
-(NULL,'Cristian','Roman',12345678,5),
-(NULL,'Aramis','Perez',12345678,1),
-(NULL,'Lucas','Brem',12345678,2)
+(NULL,3,8,'Automotor',8);
 
 INSERT INTO materias VALUES
-(NULL,4,'Santiago Trini',0,'Proyecto Informatico',1),
+(NULL,4,'Santiago Trini',0,'Proyecto Informatico',2),
 (NULL,3,'Rizzo',0,'Matematica',3),
 (NULL,2,'Guillermo Di Tieri',0,'Geografia',5),
 (NULL,2,'Jesus nazaret',0,'Matematica',5),
 (NULL,4,'De reyes',0,'Redes',2),
-(NULL,3,'Falto Diaz',1,'Educacion Fisica',3);   
+(NULL,3,'Falto Diaz',1,'Educacion Fisica',3);  
+
+INSERT INTO alumnos VALUES
+(NULL,'Luna','Estanga',12345678,2),
+(NULL,'Santiago','Villarroel',12345678,3),
+(NULL,'Cristian','Roman',12345678,5),
+(NULL,'Aramis','Perez',12345678,2),
+(NULL,'Lucas','Brem',12345678,2);
+
+
