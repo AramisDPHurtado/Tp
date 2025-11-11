7969d0d697276f162d3c4fd81b206452ace789f4
Aramis Damian Perez Hurtado <aramis.perez.hurtado@gmail.com>
2025-11-10 22:40:41 -0300

    Update main.js

--- cliente/main.js
+++ cliente/main.js
@@ -1,46 +1,51 @@
-const url = 'http://localhost:7000/api/cursos';
-
 cargarLista()
+async function llamarApi(puntoFinal,metodo,body){
+    let opciones = {
+        method: metodo,
+        headers: {"Content-Type":"application/json"}
+    }
+    let res = await fetch("http://localhost:7000/api/"+puntoFinal,opciones);
+    let data = res.json();
+    return data;
+}
+async function cargarLista(){
 
-function cargarLista(){
-
-    fetch(url)
-    .then(res => res.json())
-    .then(data =>{
+    let cursos = await llamarApi("cursos","GET");
     let tbody = document.querySelector("tbody");
     tbody.innerHTML= '';
-     for (let alumno of data) {
-    let tr = document.createElement('tr');
-    let aid = document.createElement('td');
-    let nombre = document.createElement('td');
-    let apellido = document.createElement('td');
-    let {id,nombres ,apellidos } = alumno;
-    aid.textContent=id;
-    nombre.textContent = nombres;
-    apellido.textContent = apellidos;
-    tr.append(aid, nombre , apellido);
-    const btns = ['P','A','T','AP','RA'];
-    let td = document.createElement('td');
-    for(let text of btns) {
-        let button = document.createElement('button');
-        button.textContent = text;
-        button.onclick = handleClick;
-        td.append(button);
-    }
-    tr.append(td);
-    tbody.append(tr);
+    for (let alumno of cursos) {
+        let tr = document.createElement('tr');
+        let aid = document.createElement('td');
+        let nombre = document.createElement('td');
+        let apellido = document.createElement('td');
+        let {id,nombres ,apellidos } = alumno;
+        aid.textContent=id;
+        nombre.textContent = nombres;
+        apellido.textContent = apellidos;
+        tr.append(aid, nombre , apellido);
+        const btns = ['P','A','T','AP','RA'];
+        let td = document.createElement('td');
+        for(let text of btns) {
+            let button = document.createElement('button');
+            button.textContent = text;
+            button.onclick = handleClick;
+            td.append(button);
+        }
+        tr.append(td);
+        tbody.append(tr);
 
-    }})
+    }
 }
 
-function cargarCursos() {
-    fetch('http://localhost:7000/api/cursos')
+
+ async function cargarCursos() {
+    let cursos = llamarApi("cursos","GET")
     .then(res => res.json())
     .then(data => {
         console.log(data);
         const select = document.querySelector('#cursos');
         select.innerHTML = '';
-        for (let curso of data) {
+        for (let curso of cursos) {
             const option = document.createElement('option');
             const {anio,division,esp} = curso;
             option.value = curso.id;
