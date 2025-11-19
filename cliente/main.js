9a04b5064bbb8b574090266dfa30389ae96bc8d9
Aramis Damian Perez Hurtado <aramis.perez.hurtado@gmail.com>
2025-11-18 21:45:49 -0300

    Update main.js

--- cliente/main.js
+++ cliente/main.js
@@ -1,102 +1,77 @@
-/*
-async function llamarApi(puntoFinal,metodo,body){
-    let opciones = {
-        method: metodo,
-        headers: {"Content-Type":"application/json"}
-        }
-        let res = await fetch("http://localhost:7000/api/"+puntoFinal,opciones);
-        let data = res.json();
-        return data;
-        }
-        */
-    const url = "http://localhost:7000/api/cursos";
-    cargarLista()
-       
- function cargarLista(){
+const api = "http://localhost:7000/api";
 
-    let cursos = url
-    let tbody = document.querySelector("tbody");
-    tbody.innerHTML= '';
-    for (let alumno of cursos) {
-        let tr = document.createElement('tr');
-        let aid = document.createElement('td');
-        let nombre = document.createElement('td');
-        let apellido = document.createElement('td');
-        let {id,nombres ,apellidos } = alumno;
-        aid.textContent=id;
-        nombre.textContent = nombres;
-        apellido.textContent = apellidos;
-        tr.append(aid, nombre , apellido);
-        const btns = ['P','A','T','AP','RA'];
-        let td = document.createElement('td');
-        for(let text of btns) {
-            let button = document.createElement('button');
-            button.textContent = text;
-            button.onclick = handleClick;
-            td.append(button);
-        }
-        tr.append(td);
-        tbody.append(tr);
+document.querySelector("#cursos").addEventListener("change", cargarMaterias);
+document.querySelector("#cursos").addEventListener("change", cargarAlumnos);
 
-    }
-}
-
-
- function cargarCursos() {
-    let cursos = url
-    fetch(cursos)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        const select = document.querySelector('#cursos');
-        select.innerHTML = '';
-        for (let curso of cursos) {
-            const option = document.createElement('option');
-            const {anio,division,esp} = curso;
-            option.value = curso.id;
-            option.textContent = anio + esp + division;
-            select.append(option); 
-        }
-    })
-    .catch(err => alert (err.stack));
-}
-
+// Cargar cursos al iniciar
 cargarCursos();
 
-function cargarMaterias(e) {
-    const cursoId = e.target.value;
-    fetch('http://localhost:7000/api/materias/' + cursoId)
-    .then(res => res.json())
-    .then(data => {
-        const select = document.querySelector ('#materias');
-        select.innerHTML = '';
-        for(let materia of data) {
-            const option = document.createElement('option');
-            option.textContent = materia.nombre;
-            option.value = materia.id;
-            select.append(option);
-        }
-    })
+function cargarCursos(){
+    fetch(api + "/cursos")
+    .then(res=>res.json())
+    .then(data=>{
+        const sel = document.querySelector("#cursos");
+        sel.innerHTML = "";
+        data.forEach(c=>{
+            const op = document.createElement("option");
+            op.value = c.id;
+            op.textContent = `${c.curso}°${c.division} ${c.esp}`;
+            sel.appendChild(op);
+        });
+    });
 }
 
-function handleClick(event) {
-    let row = event.target.parentElement.parentElement;
-    let idAlumno = row.children[0].textContent;
-    let select = document.querySelector('#materias');
-    let idMateria = select.value;
-    let datos = {
-        tipo: event.target.textContent,
-        alumno:idAlumno,
-        materia : idMateria
-    };
-    const options = {
-        method:'POST',
-        body: JSON.stringify(datos),
-        headers :{'Content-Type': 'application/json' }
+function cargarMaterias(){
+    let id = document.querySelector("#cursos").value;
+    fetch(api + "/materias/" + id)
+    .then(res=>res.json())
+    .then(data=>{
+        const sel = document.querySelector("#materias");
+        sel.innerHTML = "";
+        data.forEach(m=>{
+            const op = document.createElement("option");
+            op.value = m.id;
+            op.textContent = m.nombre;
+            sel.appendChild(op);
+        });
+    });
+}
 
-    };
-    fetch('http://localhost:7000/api/asistencias')
-    .then(res => res.json())
-    .then(data => alert(data))
-    .catch(err => alert(err.stack));
+function cargarAlumnos(){
+    let id = document.querySelector("#cursos").value;
+    fetch(api + "/alumnos/" + id)
+    .then(res=>res.json())
+    .then(data=>{
+        const tbody = document.querySelector("tbody");
+        tbody.innerHTML = "";
+        data.forEach(a=>{
+            const tr = document.createElement("tr");
+            tr.innerHTML = `
+                <td>${a.nombres}</td>
+                <td>${a.apellidos}</td>
+                <td>${a.dni}</td>
+            `;
+            const td = document.createElement("td");
+            ["P","A","T","AP","RA"].forEach(t=>{
+                const btn = document.createElement("button");
+                btn.textContent = t;
+                btn.onclick = ()=> registrarAsistencia(t, a.id);
+                td.appendChild(btn);
+            });
+            tr.appendChild(td);
+            tbody.appendChild(tr);
+        });
+    });
+}
+
+function registrarAsistencia(tipo, alumno){
+    const materia = document.querySelector("#materias").value;
+
+    fetch(api + "/asistencias",{
+        method:"POST",
+        headers:{"Content-Type": "application/json"},
+        body: JSON.stringify({tipo, alumno, materia})
+    })
+    .then(res=>res.json())
+    .then(msg=> alert(msg.msg));
 }
